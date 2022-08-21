aa6103b4a05650ecb5a80343b3c5ad7f2bc48bdf
srizzo8 <srizzo8@villanova.edu>
2022-08-20 23:30:53 -0400

    I ensured that my Firebase realtime database was initialized into my Flutter project and that all of the necessary dependencies for the Firebase realtime database were in the Flutter project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
-  runApp(const MyApp());
+FirebaseDatabase database = FirebaseDatabase.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
+
+
+/*void main() async{
+  runApp(const MyApp());
+}*/
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
