87146e816d697d05a3f4a28cf82fbb54d047038e
srizzo8 <srizzo8@villanova.edu>
2022-10-14 09:51:01 -0400

    I added a data table to the spectralClassPage page.

--- lib/spectralClassPage.dart
+++ lib/spectralClassPage.dart
@@ -9,6 +9,7 @@ import 'main.dart';
 
 class spectralClassPage extends StatelessWidget {
   static String nameOfRoute = '/spectralClassPage';
+  final spectralClassRef = FirebaseDatabase.instance.ref("/spectral_class");
 
   @override
   Widget build(BuildContext bc){
@@ -22,6 +23,27 @@ class spectralClassPage extends StatelessWidget {
             alignment: Alignment.topCenter,
             child: Text("Spectral Classes of Stars", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
             height: 30,
+          ),
+          DataTable(
+            columns: [
+              DataColumn(label: Text('Spectral Class', style: TextStyle(fontWeight: FontWeight.bold))),
+              DataColumn(label: Text('Temperature', style: TextStyle(fontWeight: FontWeight.bold))),
+              DataColumn(label: Text('Example', style: TextStyle(fontWeight: FontWeight.bold))),
+              DataColumn(label: Text('Number of Stars with Star Expedition Articles', style: TextStyle(fontWeight: FontWeight.bold))),
+            ],
+            rows: [
+              DataRow(cells: [
+                DataCell(Text('M')),
+                DataCell(Text('2545-3760 K')),
+                DataCell(Text('Proxima Centauri')),
+              ]),
+            ],
+            ),
+          Container(
+            child: Text("Sources", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Container(
+            child: Text("https://www.handprint.com/ASTRO/specclass.html", textAlign: TextAlign.center),
           ),
         ],
       ),
