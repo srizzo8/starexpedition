8705d75e8633697ea4d5bef1caacb3ce80346660
srizzo8 <srizzo8@villanova.edu>
2022-10-20 00:09:11 -0400

    I added rows about the K, G, F, A, B, and O spectral classes along with their temperatures and fixed the issues that involve the container with the "Sources" title only partially showing on the spectralClassPage page and the container that stores the website that I used as a source not being below the container that stores the "Sources" title on the spectralClassPage page.

--- lib/spectralClassPage.dart
+++ lib/spectralClassPage.dart
@@ -14,7 +14,7 @@ class spectralClassPage extends StatelessWidget {
   //String spectralClass = "";
 
   Future <List<String>> getSpectralClassData() async{
-    final spectralClassRef = FirebaseDatabase.instance.ref("/spectral_class");
+    final spectralClassRef = FirebaseDatabase.instance.ref().child('spectral_class');
     final spectralClassSnapshot = await spectralClassRef.get();
 
     return [spectralClassSnapshot.value.toString()];
@@ -30,6 +30,7 @@ class spectralClassPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext bc){
+    initState();
     return Scaffold(
       appBar: AppBar(
         title: Text("Star Expedition"),
@@ -43,25 +44,59 @@ class spectralClassPage extends StatelessWidget {
           ),
           DataTable(
             columns: [
-              DataColumn(label: Text('Spectral Class', style: TextStyle(fontWeight: FontWeight.bold))),
-              //DataColumn(label: Text('Temperature', style: TextStyle(fontWeight: FontWeight.bold))),
+              DataColumn(label: Text('Spectral Class', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 9.0))),
+              DataColumn(label: Text('Temperature', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 9.0))),
               //DataColumn(label: Text('Example', style: TextStyle(fontWeight: FontWeight.bold))),
-              DataColumn(label: Text('Number of Stars with Articles', style: TextStyle(fontWeight: FontWeight.bold))),
+              DataColumn(label: Text('Stars with Articles', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 9.0))),
             ],
             rows: [
               DataRow(cells: [
                 DataCell(Text('M')),
-                //DataCell(Text('2545-3760 K')),
+                DataCell(Text('2500-3800 K')),
                 //DataCell(Text('Proxima Centauri')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('K')),
+                DataCell(Text('3800-5300 K')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('G')),
+                DataCell(Text('5300-6000 K')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('F')),
+                DataCell(Text('6000-7300 K')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('A')),
+                DataCell(Text('7300-10000 K')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('B')),
+                DataCell(Text('10000-30000 K')),
+                DataCell(Text(spectralClassOfStars.toString())),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('O')),
+                DataCell(Text('30000+ K')),
                 DataCell(Text(spectralClassOfStars.toString())),
               ]),
             ],
             ),
           Container(
             child: Text("Sources", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+            height: 20,
+            width: 360,
           ),
           Container(
             child: Text("https://www.handprint.com/ASTRO/specclass.html", textAlign: TextAlign.center),
+            height: 20,
+            width: 360,
           ),
         ],
       ),
