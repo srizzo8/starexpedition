2e10ab73cfab89debf0f35805afe54773ffd0252
srizzo8 <srizzo8@villanova.edu>
2022-11-05 22:21:32 -0400

    I put an if statement that checks to see if the text boxes that represent one's username, the name of a thread, and the content of a thread are not empty and made the createThreadState page scrollable.

--- lib/createThread.dart
+++ lib/createThread.dart
@@ -17,62 +17,74 @@ class createThread extends StatefulWidget{
 
 class createThreadState extends State<createThread>{
   static String threadCreator = '/createThread';
+  final usernameController = TextEditingController();
+  final threadNameController = TextEditingController();
+  final threadContentController = TextEditingController();
 
   Widget build(BuildContext createThreadBuildContext){
     return Scaffold(
       appBar: AppBar(
         title: Text("Star Expedition"),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Text("Making a thread", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0), textAlign: TextAlign.center),
-            width: 480,
-            alignment: Alignment.center,
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Username",
-              ),
-              maxLines: 1,
-              maxLength: 30,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Thread Name",
-              ),
-              maxLines: 2,
-              maxLength: 250,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Thread Content",
-                contentPadding: EdgeInsets.symmetric(vertical: 80),
-              ),
-            )
-          ),
-          GestureDetector(
-            child: Container(
-              child: Text("Post to Subforum", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white), textAlign: TextAlign.center),
-              color: Colors.black,
-              margin: EdgeInsets.only(left: 200.0),
-              height: 30,
-              width: 140,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text("Making a thread", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0), textAlign: TextAlign.center),
+              width: 480,
               alignment: Alignment.center,
             ),
-            onTap: (){
-              print('Posting the thread');
-            }
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                ),
+                maxLines: 1,
+                maxLength: 30,
+                controller: usernameController,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Thread Name",
+                ),
+                maxLines: 2,
+                maxLength: 250,
+                controller: threadNameController,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Thread Content",
+                  contentPadding: EdgeInsets.symmetric(vertical: 80),
+                ),
+                controller: threadContentController,
+              ),
+            ),
+            GestureDetector(
+              child: Container(
+                child: Text("Post to Subforum", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white), textAlign: TextAlign.center),
+                color: Colors.black,
+                margin: EdgeInsets.only(left: 200.0),
+                height: 30,
+                width: 140,
+                alignment: Alignment.center,
+              ),
+              onTap: (){
+                //print('Posting the thread');
+                if(usernameController.text != "" && threadNameController.text != "" && threadContentController.text != ""){
+                  print(usernameController.text);
+                  print('You are ready to post this thread');
+                }
+              }
+            ),
+          ],
+        ),
       ),
     );
   }
