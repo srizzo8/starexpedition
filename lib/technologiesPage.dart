446132eef850b1b08e8e21c6db4990c7123027c6
srizzo8 <srizzo8@villanova.edu>
2024-04-03 19:16:05 -0400

    Making the back buttons for the technologiesPageState and technologiesThreadContent pages.

--- lib/technologiesPage.dart
+++ lib/technologiesPage.dart
@@ -60,6 +60,13 @@ class technologiesPageState extends State<technologiesPage>{
     return Scaffold(
       appBar: AppBar(
         title: Text("Star Expedition"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () =>{
+            Navigator.pushNamed(context, '/discussionBoardPage'),
+          }
+        ),
       ),
       body: Column(
         children: <Widget>[
@@ -126,6 +133,13 @@ class technologiesThreadContent extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text("Star Expedition"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () =>{
+            Navigator.pushNamed(context, '/discussionBoardPage'),
+          }
+        ),
       ),
       body: Wrap(
         children: <Widget>[
