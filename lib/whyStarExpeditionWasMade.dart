bb3c5433ea706f16623e7da098d82ad6cc90e00c
srizzo8 <srizzo8@villanova.edu>
2024-04-08 18:20:36 -0400

    Revising the Why Star Expedition Was Made page.

--- lib/whyStarExpeditionWasMade.dart
+++ lib/whyStarExpeditionWasMade.dart
@@ -34,17 +34,25 @@ class whyStarExpeditionWasMadePageState extends State<whyStarExpeditionWasMadePa
       appBar: AppBar(
         title: Text("Star Expedition"),
       ),
-      body: Container(
-        child: Text("As a young child, I always had an interest in the Universe and all of its celestial bodies." +
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Text("Why Star Expedition Was Made",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Container(
+            child: Text("As a young child, I always had an interest in the Universe and all of its celestial bodies." +
             "However, one thing that interested me the most was how stars and planets can be capable of supporting " +
             "life for flora and fauna. This can take millions and even billions of years to accomplish, depending on " +
-            "the evolution of the star, planet, or both." + "\n" +
+            "the evolution of the star, planet, or both." + "\n" + "\n" +
             "The formation of our Solar System was something that I especially found fascinating." +
             "It is magnificient how the Sun was born, how it resulted in creating Earth, and how it ultimately made Earth evolve the way that it did. " +
             "It is also intriguing to realize how far nature and civilization have progressed over the past 4.6 billion years."
             "I truly wonder if there are other suns and other Earths out there." +
             "I hope that Star Expedition can be helpful in providing people with information about " +
             "nearby stars that have confirmed terrestrial planets that can potentially support life for the flora and fauna found in Earth."),
+          ),
+        ]
       ),
       drawer: myMain.starExpeditionNavigationDrawer(),
     );
