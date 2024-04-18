118a3c9f123837fbf184112c16f93d55b23e4f81
srizzo8 <srizzo8@villanova.edu>
2024-04-17 21:18:48 -0400

    Making the back button in the login page lead users from the login page to the home page.

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -59,6 +59,13 @@ class loginPageState extends State<loginPage>{
     return Scaffold(
       appBar: AppBar(
         title: Text("Star Expedition"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () =>{
+            Navigator.push(bc, MaterialPageRoute(builder: (bc) => const myMain.StarExpedition())),
+          }
+        )
       ),
       body: Wrap(
         children: <Widget>[
