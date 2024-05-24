a91ee0863e3912487903e749622b66a94227042d
srizzo8 <srizzo8@villanova.edu>
2024-05-24 16:17:40 -0400

    Using Uri to send "Welcome to Star Expedition!" emails to new users (even though they are sending them to themselves).

--- lib/emailNotifications.dart
+++ lib/emailNotifications.dart
@@ -7,12 +7,19 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mailer/mailer.dart';
-import 'package:mailer/smtp_server/gmail.dart';
+import 'package:url_launcher/url_launcher.dart';
+//import 'package:mailer/mailer.dart';
+//import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+//import 'package:flutter_email_sender/flutter_email_sender.dart';
 //import 'package:backendless_sdk/backendless_sdk.dart';
 import 'googleAuthApi.dart';
 import 'main.dart' as myMain;
 import 'registerPage.dart' as registerPage;
+//import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 class emailNotifications extends StatelessWidget {
   const emailNotifications ({Key? key}) : super(key: key);
@@ -25,31 +32,19 @@ class emailNotifications extends StatelessWidget {
   }
 }
 
-Future registrationConfirmationEmail() async{
-  final myUser = await GoogleAuthApi.signIn();
-
-  if(myUser == null){
-    return;
+Future<void> registrationConfirmationEmail() async{
+  final Uri par = Uri(
+      scheme: 'mailto',
+      path: registerPage.myNewEmail,
+      queryParameters: {
+        'subject': 'Welcome to Star Expedition!',
+        'body': 'I hope you enjoy your time on here!'
+      }
+  );
+  if(await canLaunchUrl(par)){
+    await launchUrl(par);
   }
-
-  final myEmail = myUser?.email;
-  final au = await myUser?.authentication;
-  final token = au?.accessToken;
-
-  print("Howdy partner! Nice to meet you! Your email ${myEmail} has been authenticated!");
-
-  final mySmtpServer = gmailSaslXoauth2(myEmail!, token!);
-
-  final emailInfo = Message()
-    ..from = Address(myEmail)
-    ..recipients = [registerPage.myNewEmail]
-    ..subject = "Welcome to Star Expedition"
-    ..text = "I hope you enjoy Star Expedition!";
-
-  try{
-    await send(emailInfo, mySmtpServer);
-  } on MailerException catch(e){
-    print("This is e: ${e}");
+  else{
+    print("Unable to launch the Url, which is ${par.toString()}");
   }
 }
-
