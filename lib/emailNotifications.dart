ccebbd5db26ac97ff2ef533774499430b76e1a26
srizzo8 <srizzo8@villanova.edu>
2024-05-26 19:51:42 -0400

    Fixing the issue where the build fails (probably because of the Kotlin and build:gradle versions).

--- lib/emailNotifications.dart
+++ lib/emailNotifications.dart
@@ -10,11 +10,11 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mailer/mailer.dart';
-import 'package:url_launcher/url_launcher.dart';
+//import 'package:url_launcher/url_launcher.dart';
 //import 'package:mailer/mailer.dart';
 //import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
-//import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 //import 'package:backendless_sdk/backendless_sdk.dart';
 import 'googleAuthApi.dart';
 import 'main.dart' as myMain;
@@ -33,7 +33,7 @@ class emailNotifications extends StatelessWidget {
 }
 
 Future<void> registrationConfirmationEmail() async{
-  final Uri par = Uri(
+  /*final Uri par = Uri(
       scheme: 'mailto',
       path: registerPage.myNewEmail,
       queryParameters: {
@@ -46,5 +46,16 @@ Future<void> registrationConfirmationEmail() async{
   }
   else{
     print("Unable to launch the Url, which is ${par.toString()}");
-  }
+  }*/
+
+  final Email myEmail = Email(
+    subject: "Welcome to Star Expedition!",
+    body: "I hope you enjoy your time on here!",
+    recipients: [registerPage.myNewUsername],
+    isHTML: false
+  );
+
+  print("Sending email");
+
+  await FlutterEmailSender.send(myEmail);
 }
