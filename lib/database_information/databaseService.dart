906691d494c335dbaa5f810ea7e936df57534475
srizzo8 <srizzo8@villanova.edu>
2025-10-13 16:40:23 -0400

    Trying to get Star Expedition to work on Desktop like it does for Chrome (part 7 - fixing the "Unhandled Exception: Null check operator used on a null value" issue that occurs when a user clicks on the "Sign Up" button on Desktop)

--- lib/database_information/databaseService.dart
+++ lib/database_information/databaseService.dart
@@ -5,8 +5,10 @@ import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:starexpedition4/database_information/usersDatabaseInfo.dart';
 import 'package:starexpedition4/registerPage.dart' as registerPage;
+import 'package:starexpedition4/firebaseDesktopHelper.dart';
 
 class databaseService{
   static databaseService dbs = databaseService.internal();
@@ -49,16 +51,35 @@ class databaseService{
       return db!;
     }
     try{
-      var dbPath = await getApplicationDocumentsDirectory();
-      String path = join(dbPath.path, "users.db");
-      log(path);
-      //onCreate: onc,
-      return await openDatabase(
+      if(firebaseDesktopHelper.onDesktop){
+        sqfliteFfiInit();
+        databaseFactory = databaseFactoryFfi;
+
+        var dbPath = await getApplicationDocumentsDirectory();
+        String path = join(dbPath.path, "users.db");
+        log("Path for desktop: $path");
+        //onCreate: onc,
+        return await openDatabase(
+            path,
+            version: 1,
+            onCreate: (Database myDb, int version) async {
+              await myDb.execute(usersTable);
+            }
+        );
+      }
+      else{
+        var dbPath = await getApplicationDocumentsDirectory();
+        String path = join(dbPath.path, "users.db");
+        log(path);
+        //onCreate: onc,
+        return await openDatabase(
           path,
           version: 1,
-          onCreate: (Database myDb, int version) async{
+          onCreate: (Database myDb, int version) async {
             await myDb.execute(usersTable);
-          });
+          }
+        );
+      }
     }
     catch (e){
       print("This is e: $e");
