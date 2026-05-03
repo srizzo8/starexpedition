43326298f9e9bdd27afb471e7f4e4ae890c186a5
srizzo8 <srizzo8@villanova.edu>
2026-05-03 18:15:02 -0400

    Making sure that if a trial expires for a user and a user removes Star Expedition from his or her device, Star Expedition will still show him or her that his or her trial has expired if he or she reinstalls Star Expedition on his or her device

--- lib/trials_and_payments/theTrialService.dart
+++ lib/trials_and_payments/theTrialService.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'dart:math';
 //import 'dart:html';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -24,41 +26,71 @@ class theTrialService{
   static const String myInstallDateKey = "installDate";
   static const int myTrialDays = 7;
 
+  //Getting the id of one's device:
+  Future<String> getDeviceId() async{
+    final myDeviceInfo = DeviceInfoPlugin();
+    final myAndroidInfo = await myDeviceInfo.androidInfo;
+
+    return myAndroidInfo.id!;
+  }
+
   //Called once; this records the installation date:
   Future<void> getInstallationDate() async{
-    final myPreferences = await SharedPreferences.getInstance();
-    if(myPreferences.getString(myInstallDateKey) == null){
-      await myPreferences.setString(myInstallDateKey, DateTime.now().toIso8601String(),);
+    final myDeviceId = await getDeviceId();
+
+    //Checking Firestore to see if the device has been seen previously:
+    final myDoc = await FirebaseFirestore.instance.collection("Trials").doc(myDeviceId).get();
+
+    if(myDoc.exists){
+      //The device already has a record of using the app, and should be saved:
+      final myPrefs = await SharedPreferences.getInstance();
+      await myPrefs.setString(myInstallDateKey, myDoc.data()!["installDate"]);
+    }
+    else{
+      //The first time the device is using the app:
+      final currentTime = DateTime.now().toIso8601String();
+
+      //Permanently saving device info to Firestore:
+      await FirebaseFirestore.instance.collection("Trials").doc(myDeviceId).set({"installDate": currentTime, "deviceId": myDeviceId});
+
+      //Saving information locally:
+      final myPrefs = await SharedPreferences.getInstance();
+      await myPrefs.setString(myInstallDateKey, currentTime);
     }
   }
 
   //Returns true if a user's trial is still in the one-week time window:
   Future<bool> isInTrial() async{
-    final myPrefs = await SharedPreferences.getInstance();
-    final myInstallDateString = myPrefs.getString(myInstallDateKey);
+    final myDeviceId = await getDeviceId();
 
-    if(myInstallDateString == null){
+    //Checking Firestore database:
+    final myDoc = await FirebaseFirestore.instance.collection("Trials").doc(myDeviceId).get();
+
+    if(!myDoc.exists){
       return false;
     }
 
-    final myInstallDate = DateTime.parse(myInstallDateString);
+    final myInstallDate = DateTime.parse(myDoc.data()!["installDate"]);
     final daysSinceInstall = DateTime.now().difference(myInstallDate).inDays;
+    final inTrial = daysSinceInstall < myTrialDays;
 
-    return daysSinceInstall < myTrialDays;
+    return inTrial;
   }
 
-  //Returns the number of trial days that are remaining for a user:
-  Future<int> remainingTrialDays() async{
-    final myPrefs = await SharedPreferences.getInstance();
-    final myInstallDateString = myPrefs.getString(myInstallDateKey);
+  //Checking how many days are left for a user's trial:
+  Future<int> daysLeftOfTrial() async{
+    final myDeviceId = await getDeviceId();
 
-    if(myInstallDateString == null){
+    final myDoc = await FirebaseFirestore.instance.collection("Trials").doc(myDeviceId).get();
+
+    if(!myDoc.exists){
       return 0;
     }
 
-    final myInstallDate = DateTime.parse(myInstallDateString);
+    final myInstallDate = DateTime.parse(myDoc.data()!["installDate"]);
     final daysSinceInstall = DateTime.now().difference(myInstallDate).inDays;
+    final myDaysLeft = (myTrialDays - daysSinceInstall).clamp(0, myTrialDays);
 
-    return (myTrialDays - daysSinceInstall).clamp(0, myTrialDays);
+    return myDaysLeft;
   }
 }
